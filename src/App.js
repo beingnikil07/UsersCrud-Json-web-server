741195fce85e1b6151952ac938a4b121347c2885
Nikil kumar <kumarnikil220@gmail.com>
2023-01-05 21:35:20 -0800

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact'
+import About from './components/pages/About'
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Navbar />
+      <Home />
+      <Contact />
+      <About />
     </div>
   );
 }
