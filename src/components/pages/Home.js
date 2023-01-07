cc6e6cdaac7723f7b0d2036cb0d53ba1aa519dd2
Nikil kumar <kumarnikil220@gmail.com>
2023-01-06 21:44:50 -0800

    fetching data from json server api using axios

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const Home = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    loadUsers();
+  }, []);
 
-const Home=()=>{
-    return(
-       <div className='container'>
-          <div className='py-4'>
-            <h1>Home Page </h1>
-          </div>
-       </div>
+  let loadUsers = async () => {
+    const result = await axios.get("http://localhost:3001/users");
+    setUsers(result.data);
+  };
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">User Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+           {
+            users.map((user,index)=>(
+               <tr>
+                  <th scope="row">{index+1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+               </tr>
+            ))
+           }
 
-    )
-}
+
+
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  );
+};
 
 export default Home;
